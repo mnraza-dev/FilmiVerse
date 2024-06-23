5b01906b1fde3fdf9e5aeda3511f9e62aaeab401
mnrazablog <mnraza94@gmail.com>
2024-06-23 12:45:37 +0530

    react router dom is configured and added shadcn for rapid UI development

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { Button } from "@/components/ui/button";
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Actors,
+  Movies,
+  MovieInformation,
+  Navbar,
+  Profile,
+} from "@/components";
 
 const App = () => {
   return (
-    <Button>Click me</Button>
-  )
-}
+    <>
+      <Navbar />
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/"  element={<Movies />} />
+            <Route path="/actors/:id" element={<Actors />} />
+            <Route path="/movies/:id" element={<MovieInformation />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </>
+  );
+};
 
-export default App
+export default App;
